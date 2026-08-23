b467545eda0964ae162fc21e3cc5f332061c6961
ketamine <vladg3@mail.ru>
2026-08-23 15:44:52 +0700

    initial commit

---
+++ ChillChat/ChillChat/ChillChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChillChatApp.swift
+//  ChillChat
+//
+//  Created by Vladislav Mishukov on 23.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChillChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChillChat/ChillChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChillChat
+//
+//  Created by Vladislav Mishukov on 23.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
