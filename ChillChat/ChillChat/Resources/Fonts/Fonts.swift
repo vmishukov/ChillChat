6241f6a8887c546c134bb883faafdd320f4f0d3f
ketamine <vladg3@mail.ru>
2026-09-01 13:07:12 +0700

    Feature: start working on app ui

---
+++ ChillChat/ChillChat/Resources/Fonts/Fonts.swift
@@ -0,0 +1,18 @@
+//
+//  Fonts.swift
+//  ChillChat
+//
+//  Created by Vladislav Mishukov on 01.09.2026.
+//
+
+import Foundation
+
+enum Fonts: String {
+    case soraThin = "Sora-Thin"
+    case soraExtraLight = "Sora-Regular_ExtraLight"
+    case soraLight = "Sora-Regular_Light"
+    case soraRegular = "Sora-Regular"
+    case soraSemiBold = "Sora-Regular_SemiBold"
+    case soraBold = "Sora-Regular_Bold"
+    case soraExtraBold = "Sora-Regular_ExtraBold"
+}
